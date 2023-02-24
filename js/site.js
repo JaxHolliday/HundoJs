9c2cb8e88d47485fb3cd74c21a5a9a156ab7554d
Jackson Holliday <jaxholliday@gmail.com>
2023-02-24 17:55:09 -0500

    first function I

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//call Hello Word 
-function HelloWorld(){
-    alert("Hello World");
+//get values from the UI or user inputted numbers 
+//starts or controller function
+function getValues(){
+    //get values from the page 
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value: " + startValue);
+    //call generate numbers
+
+    //call displayNumbers
+}
+
+//generate numbers from start and ending values 
+//logic function(s)
+function generateNumbers(){
+
+}
+
+//display the output and mark even numbers BOLD
+//display functions 
+function displayNumbers(){
+
 }
